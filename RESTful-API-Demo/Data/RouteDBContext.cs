9437cb17607aa650ceb8ad5f5a7fdba81c284c99
Leon <leon.id@qq.com>
2020-03-03 21:56:45 +0800

    Seed data;

--- RESTful-API-Demo/Data/RouteDBContext.cs
+++ RESTful-API-Demo/Data/RouteDBContext.cs
@@ -29,6 +29,26 @@ namespace RESTful_API_Demo.Data
             modelBuilder.Entity<Employee>().Property(x => x.LastName).IsRequired().HasMaxLength(50);
 
             modelBuilder.Entity<Employee>().HasOne(x => x.Company).WithMany(x => x.Employees).HasForeignKey(x => x.CompanyId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Company>().HasData(
+                new Company
+                {
+                    Id = Guid.Parse("bbdee09c-089b-4d30-bece-44df5923716c"),
+                    Name = "Microsoft",
+                    Introduction = "Great Company",
+                },
+                new Company
+                {
+                    Id = Guid.Parse("6fb600c1-9011-4fd7-9234-881379716440"),
+                    Name = "Google",
+                    Introduction = "Don't be evil",
+                },
+                new Company
+                {
+                    Id = Guid.Parse("5efc910b-2f45-43df-afae-620d40542853"),
+                    Name = "Alibaba",
+                    Introduction = "Fubao Company",
+                });
         }
     }
 }
