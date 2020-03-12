72c2b3955078fd1791a3f314b2a4b274d4ea1d39
Leon <leon.id@qq.com>
2020-03-12 21:25:42 +0800

    自定义模型验证错误信息；

--- RESTful-API-Demo/Startup.cs
+++ RESTful-API-Demo/Startup.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -25,12 +26,35 @@ namespace RESTful_API_Demo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers(setup =>
-            {
-                // 自动对无法接受的 Accept Header 返回 406 代码
-                setup.ReturnHttpNotAcceptable = true;
-            })
-                .AddXmlDataContractSerializerFormatters();
+            services
+                .AddControllers(setup =>
+                {
+                    // 自动对无法接受的 Accept Header 返回 406 代码
+                    setup.ReturnHttpNotAcceptable = true;
+                })
+                // 启用 XML 格式支持
+                .AddXmlDataContractSerializerFormatters()
+                .ConfigureApiBehaviorOptions(setup =>
+                {
+                    // 自定义模型验证错误信息
+                    setup.InvalidModelStateResponseFactory = context =>
+                    {
+                        var detail = new ValidationProblemDetails(context.ModelState)
+                        {
+                            Type = "https://github.com/CuteLeon",
+                            Title = "发生异常",
+                            Status = StatusCodes.Status422UnprocessableEntity,
+                            Detail = "详细信息",
+                            Instance = context.HttpContext.Request.Path,
+                        };
+                        detail.Extensions.Add("TraceId", context.HttpContext.TraceIdentifier);
+
+                        return new UnprocessableEntityObjectResult(detail)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    };
+                });
 
             // 使 AutoMapper 自动发现应用域内的类型映射配置类 (Profile)
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
